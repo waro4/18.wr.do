83e701f79e114470db43348e38a464a10b906da5
oiov <oiov@duck.com>
2025-02-19 18:46:27 +0800

    upd8

--- routes/about.tsx
+++ routes/about.tsx
@@ -4,10 +4,11 @@ export default function About() {
   return (
     <div class="p-2 w-full leading-8 min-h-screen text-shadow bg-[#f9e4f0] text-lg text-red-100">
       <div class="mx-auto flex max-w-screen-md flex-col items-center justify-center">
-        <div class="flex w-full items-center p-2">
-          <a class="flex-1" href="/">
+        <div class="flex w-full gap-3 justify-start items-center p-2">
+          <a class="" href="/">
             <img src="/logo.png" class="h-12 w-12" />
           </a>
+          <h1 class="font-bold text-[#c5005a]">情趣小游戏</h1>{" "}
         </div>
 
         <div class="my-6 text-[#c5005a] bg-[#f9e4f0] border border-base-300 p-4 rounded-lg shadow-lg">
@@ -16,20 +17,20 @@ export default function About() {
           <p class="my-4 text-neutral text-sm"></p>{" "}
           <ul class="list-disc list-inside text-left">
             {" "}
-            <li>解锁 100+ 姿势图片</li>{" "}
-            <li>开通同居, 进阶, 私密, SM, 户外, 男仆, 女仆 飞行棋/任务关卡</li>
+            <li>解锁 100+ 姿势图片</li> <li>开通所有飞行棋/任务关卡主题 </li>
             <li>永久解锁本应用所有功能</li>
           </ul>
           <button class="btn btn-primary text-white mt-3 px-4 py-2 border rounded-lg bg-pink-600">
             扫描下方二维码支付 9.9元
           </button>
-          <img class="my-3 rounded-lg max-w-[250px]" src="/qrcode.JPG" alt="" />
+          <p class="m-2 font-bold text-[#c5005a]">
+            🔔 支付后请添加客服微信：oiovdev，手动为您开通会员!
+          </p>
+          <img class="my-3 rounded-lg max-w-[300px]" src="/qrcode.JPG" alt="" />
         </div>
-
-        <p class="mx-2 font-bold text-[#c5005a]">
-          🔔 支付后请添加客服微信：oiovdev，手动为您开通会员!
-        </p>
       </div>
+
+      <p class="m-2 text-center font-bold text-[#c5005a]">@18.wr.do</p>
 
       <Dock />
     </div>
